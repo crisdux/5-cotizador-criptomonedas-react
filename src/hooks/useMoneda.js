e387b4924b8207f92d04253362f989fc6ff601fa
cris.dux <crisfer.4217@gmail.com>
2020-11-19 02:14:47 -0400

    Styled Components al Custom Hook

--- src/hooks/useMoneda.js
+++ src/hooks/useMoneda.js
@@ -1,18 +1,42 @@
 import React,{useState} from 'react';
+import styled from '@emotion/styled';
 
+
+const Label = styled.label`
+    font-family: 'Bebas Neue', cursive;
+    color: #fff;
+    text-transform: uppercase;
+    font-weight:bold;
+    font-size:2.4rem;
+    margin-top:2rem;
+    display:block;
+`;
+
+const Select = styled.select`
+    width:100%;
+    display:block;
+    padding:1rem;
+    --webkit-appearance:none;
+    border-radius:10px;
+    border:none;
+    font-size:1.2rem;
+`
 const useMoneda = (label,stateInicial,opciones) => {
 
     const [state, actualizarState] = useState(stateInicial);
 
     const Seleccionar = () => (
         <>
-            <label>{label}}</label>
-            <select>
+            <Label>{label}</Label>
+            <Select
+                onChange={(e) => actualizarState(e.target.value) }
+                value={state}
+            >
                 <option value="">--Seleccione una moneda--</option>
                 {opciones.map(opcion => (
                     <option key={opcion.codigo} value={opcion.codigo}>{opcion.nombre}</option>
                 ))}
-            </select>
+            </Select>
         </>
     );
 
