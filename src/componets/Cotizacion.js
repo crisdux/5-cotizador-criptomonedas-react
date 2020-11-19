ca846a886bba543864c47becc357b4ff5d79bae5
cris.dux <crisfer.4217@gmail.com>
2020-11-19 15:39:55 -0400

    CSS al Resumen

--- src/componets/Cotizacion.js
+++ src/componets/Cotizacion.js
@@ -1,16 +1,35 @@
 import React,{Fragment} from 'react'
+import styled from '@emotion/styled';
 
+const ResultadoDiv = styled.div`
+    color:#fff;
+    font-family: Arial, Helvetica, sans-serif;
+`;
+
+const Info = styled.p`
+    font-size:18px;
+    span{
+        font-weight:bold;
+    }
+`;
+
+const Precio = styled.p`
+    font-size:30px;
+    span{
+        font-weight:bold;
+    }
+`;
 function Cotizacion({resultado}) {
 
     if(Object.keys(resultado).length === 0 ) return null;
     return (
-        <React.Fragment>
-            <p>El precio es: <span>{resultado.PRICE}</span></p>
-            <p>Precio mas alto del dia: <span>{resultado.HIGHDAY}</span></p>
-            <p>Precio mas bajo del dia: <span>{resultado.LOWDAY}</span></p>
-            <p>Variacón ultimas 24 horas<span>{resultado.CHANGEPCT24HOUR}</span></p>
-            <p>Ultima actualizacion<span>{resultado.LASTUPDATE}</span></p>
-        </React.Fragment>
+        <ResultadoDiv>
+            <Precio>El precio es: <span>{resultado.PRICE}</span></Precio>
+            <Info>Precio mas alto del dia: <span>{resultado.HIGHDAY}</span></Info>
+            <Info>Precio mas bajo del dia: <span>{resultado.LOWDAY}</span></Info>
+            <Info>Variacón ultimas 24 horas<span>{resultado.CHANGEPCT24HOUR}</span></Info>
+            <Info>Ultima actualizacion<span>{resultado.LASTUPDATE}</span></Info>
+        </ResultadoDiv>
     )
 }
 
